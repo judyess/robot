1ac5048b3e57e72ca54ce20a840888089f65a069
Judy S <judysoukkhaphon@gmail.com>
2024-10-31 00:41:32 -0400

    added an array to the Link class to keep track of all existing Link objects, added limits for the coordinate plane to reflect the physical arms working space, working on how to define link position in terms of its relationship to other links

--- linksModel/linksModel.py
+++ linksModel/linksModel.py
@@ -1,5 +1,7 @@
-# to run in VSC, disconnect from remote WSL connection. Bottom right should say {}Python 3.12.2. idk how to find it again, but make sure the Kernel is the local Python 3.12
-# these are provided examples that I'm fiddling with
+"""
+Link.allLinks = array of all Link objects
+
+"""
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -10,31 +12,51 @@ import math
 
 
 class Link:
+
+    allLinks = []
     def __init__(self, pin, origin, endpoint, linkLength):
+
         self.pin = pin
         self.origin = origin
         self.endpoint = endpoint
         self.linkLength = linkLength
+        Link.allLinks.append(self) # any new Link object is added to a list. The list is going to be used for defining relationships between links and stuff.
+        """
+        if(self.pin != 0):
+            for link in Link.allLinks:              # In-Progress. Need to figure out how to consider angles when defining positions. Ugh.
+                if link.pin == self.pin -1:
+                    self.origin = Link.endpoint
+                else:
+                    self.origin = self.linkLength
+        """
+
 
 # defining the robit; Need to define next links origin == last links endpoint
 link1 = Link(0, (1, 2,0), (5, 6,0), 4)
 link2 = Link(1, (5,6,0), (3,4,0), 3.75)
 
-arm = [[(link1.origin), (link1.endpoint)], [link2.origin, link2.endpoint]]
+arm = []
 
+for link in Link.allLinks:
+    arm.append([link.origin, link.endpoint])
+
+print(arm)
 
 # plot stuff
 fig = plt.figure()
-ax2 = fig.add_subplot(projection='3d')
+ax = fig.add_subplot(projection='3d')
 
 for link in arm:
-    x, y, z = zip(*link)
-    ax2.plot(x, y, z)
-    ax2.scatter(x, y, z, c='red', s=100)
+    x, y, z = zip(*link)                                                        # Zip
+    ax.plot(x, y, z)
+    ax.scatter(x, y, z, c='red', s=100)
 
 # Graph stuff
-ax2.set_xlabel('X')
-ax2.set_ylabel('Y')
-ax2.set_zlabel('Z')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_xlim(0, 15)
+ax.set_ylim(0, 15)
+ax.set_zlim(0, 15)
 
 plt.show()
