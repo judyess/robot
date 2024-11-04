ff345ba91a935053aa4668b8c08eea19779a9cb8
Judy S <judysoukkhaphon@gmail.com>
2024-11-04 10:59:50 -0500

    fixed math function to correctly return new positions. Was trying to figure out how to make the math work with matrices, but just used cos/sins instead. Works.

--- linksModel/linksModel.py
+++ linksModel/linksModel.py
@@ -4,6 +4,12 @@ Writing a model of my robot project because physically testing, retesting and
 Also, learning how to use Matplotlib is cool too.
 
 Link.allLinks = array of all Link objects
+
+To-Do: 
+1. Find a way to map links to eachother better 
+    instead of looping through the entire list of links
+2. Figure out which way to display positions
+3. Loop through all links at once and call rotate -- or something better
 """
 
 from matplotlib import pyplot as plt
@@ -18,24 +24,13 @@ fig = plt.figure()
 ax = fig.add_subplot()
 #ax = fig.add_subplot(projection='3d')
 class Link:
-
     allLinks = []
     def __init__(self, pin, origin, endpoint, linkLength):
-
         self.pin = pin
         self.origin = origin
         self.endpoint = endpoint
         self.linkLength = linkLength
         Link.allLinks.append(self) # any new Link object is added to a list. The list is going to be used for defining relationships between links and stuff.
-        """
-        if(self.pin != 0):
-            for link in Link.allLinks:              # In-Progress. Need to figure out how to consider angles when defining positions. Ugh.
-                if link.pin == self.pin -1:
-                    self.origin = Link.endpoint
-                else:
-                    self.origin = self.linkLength
-        """
-
 
 
 # defining the robit; Need to define next links origin == last links endpoint
@@ -54,70 +49,71 @@ for link in arm:
     ax.scatter(x, y, z, c='red', s=100) # Just adds a dot to the points
 '''
 # a 2D plotter for ease of view in the graph while testing the math
-for link in arm:
-    x, y, z = zip(*link)                                                        # Zip
-    ax.plot(x, y)
-    ax.scatter(x, y, c='red', s=100) # Just adds a dot to the points
 
+def printArm():
+    for link in arm:
+        x, y, z = zip(*link)                                                        # Zip
+        ax.plot(x, y)
+        ax.scatter(x, y, c='red', s=100) # Just adds a dot to the points
 
-# probably an excessive thing
-class Rotation:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-"""
-    Math part to-do: 
-    1. rotate around a point
-    
-    90 degree rotation matrix from 0,0:
-    [-1, 0, 0]
-    [ 0, 1, 0]
-    [ 0, 0, 0]
-"""
-rotate90 = Rotation((np.array([-1,0,0])),(np.array([0,1,0])),np.array(([0,0,0])))
 
 
 newLinks = []
-# trying to rotate 2 arms by 90 degrees. Angle is hardcoded. 
-# currently rotates links individually by 90 degrees, but still from an origin of 0,0. (so each point is acting like a link from 0,0 to its EP)
+# I think this is correct. 
+# Need to redefine how the links are related and loop through all links in the arm.
+# angle is hard coded
 def rotateLink(link):
     print("rotating")
     print(link.endpoint)
-    if link.pin != 0:
-        previousX = Link.allLinks[link.pin-2].origin[0]
-        previousY = Link.allLinks[link.pin-2].origin[1]
-    else:
-        previousX = 0
-        previousY = 0
-    # 2nd new point is wrong
-    newX = rotate90.x * link.endpoint[0] #+ (link.origin[0] - previousX)
-    newY = rotate90.y * link.endpoint[1] #+ abs(link.origin[0] - link.origin[1])
+    for linki in Link.allLinks:
+        if linki.pin == link.pin - 1:
+            previousX = linki.origin[0]
+            previousY = linki.origin[1]
+        else:
+            previousX = 0
+            previousY = 0
 
+    x1 = previousX
+    y1 = previousY
+    x2 = link.endpoint[0]
+    y2 = link.endpoint[1]
+    x = abs(x2 - x1)
+    y = abs(y2 - y1)
+    theta = 45
+    radians = (theta*math.pi)/180
+    # rotates a single link, then adds it back to the arm (using x1, y1)
+    newX = ((x)*math.cos(radians)) - ((y)*math.sin(radians)) + x1
+    newY = ((x)*math.sin(radians)) + ((y)*math.cos(radians)) + y1
+    
     if link.pin >= 1:
         newOrigin = newLinks[len(newLinks) - 1].endpoint
     else:
         newOrigin = link.origin
 
-    newLink = Link(len(newLinks)+1, newOrigin, (newX[0], newY[1], 0), 0)
+    newLink = Link(len(newLinks)+1, newOrigin, (newX, newY, 0), 0)
     newLinks.append(newLink)
-    print(newLink.origin,", ", newLink.endpoint)
     plotLink(newLink)
 
+
+
 def plotLink(link): #correctly connects the previous link to the current link
-    if link.pin > 1:
-        previousLink = newLinks[len(newLinks) - 1]
-    else:
-        previousLink = link
+    for linki in Link.allLinks:
+        if linki.pin == link.pin - 1:
+            previousLink = linki
+        else:
+            previousLink = link
 
     x = previousLink.origin[0]
     y = previousLink.origin[1]
     x2 = link.endpoint[0]
     y2 = link.endpoint[1]
-    print(x2, ", ", y2)
+    print("new points: ", x2, ", ", y2)
     plt.plot([x,x2], [y, y2])
     plt.scatter((x, x2), (y, y2), c='black', s=100)
-    
+
+
+printArm()
+
 rotateLink(link1)
 rotateLink(link2)
 
@@ -126,8 +122,8 @@ rotateLink(link2)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 #ax.set_zlabel('Z')
-ax.set_xlim(-6, 10)
-ax.set_ylim(-6, 10)
+ax.set_xlim(-9, 10)
+ax.set_ylim(-9, 10)
 #ax.set_zlim(-10, 10)
 
 plt.show()
