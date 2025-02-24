b62afdb4448ddc271a0484e0d7acfb2755b84f62
Judy S <judysoukkhaphon@gmail.com>
2025-02-23 20:02:21 -0500

    clean up

--- wirelessControl/RobotESP32Receiver/RobotESP32Receiver.ino
+++ wirelessControl/RobotESP32Receiver/RobotESP32Receiver.ino
@@ -1,37 +1,22 @@
-/* Receiver that controls a 6DOF robot arm with all revolute joints. Connected to the transmitter through WiFi and the receivers Mac address.
-This code moves a motor by calculating where a motor should be, and sets its position over and over again.
-Position in degrees is referred to in terms of "ticks". 
+/* Receiver that controls a 6DOF robot arm with all revolute joints. Connected to the transmitter with the receivers Mac address.
+Position is in terms of "ticks". 
 
 I am using 6 ~25kg motors.
- Supplying 7.8 V to the PCA 9685 at the terminal block with a variable DC power supply.
+ Supplying 11-12 V to the PCA 9685 at the terminal block with a variable DC power supply.
  If robot doesn't move to its starting position on power up and you know they are working, 
   it is possible that the motors are not getting enough power.
-
-
-PCA 9685 and the Receiving ESP32's pin connections:
-GND . GND
-DE not used.
-SCL . IO 22 or 21 idr, my esp32 pin labels are under the board which is now soldered in place. But these are the IO pins for SCL and SDA. 
-SDA . IO 21 or 22 idr
-VCC . 3.3V 
-V+ not used. Don't use.
 */
 
 #include <esp_now.h>
 #include <WiFi.h> // this wireless controller uses WiFi
 #include <Wire.h> //for I2C communication
 #include <Adafruit_PWMServoDriver.h> // for the PCA9685
-
-// The frequency is usually given in the data sheet if one is available.
 #define MIN_PULSE_WIDTH       500 
 #define MAX_PULSE_WIDTH       2500 
 #define FREQUENCY             60
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
-//static QueueHandle_t queue;
-//static TaskHandle_t taskB;
-//int queueLength = 2; 
 
-// same as the transmitter
+// same as on the transmitter sketch for the controller
 int j1x = 601;
 int j1y = 602;
 int j1btn = 603;
@@ -40,20 +25,6 @@ int j2y = 702;
 int j2btn = 703;
 int btn1 = 310;
 int btn2 = 320;
-int btn3 = 330;
-int btn4 = 340;
-int btn5 = 350;
-int btn6 = 360;
-
-int count =2;
-float posA = 400;
-float posB = 400;
-float posC = 400;
-float posD = 400;
-float posE = 400;
-float posF = 400;
-float len;         
-float jointPosition;  
 
 // *define the same data structure sent by the transmitter*
 typedef struct data_struct {
@@ -64,7 +35,7 @@ typedef struct data_struct {
 
 // defines a joint object. (a joint is a link and its associated motor)
 struct joint{
-  int pca9685pin;              
+  int onPCA9685pin;              
   float len;         
   float jointPosition;  
   float min;
@@ -73,12 +44,12 @@ struct joint{
   float direction;
 };
 
-joint base = {0, 4, posA, 100, 700, 5, 1};    // rotate
-joint link1 = {1, 5, posB, 150, 700, 5, 1};   // bend reverse of link2
-joint link2 = {2, 3.5, posC, 150, 700, 5, 1}; // bend reverse of link1
-joint link3 = {3, 1, posD, 150, 700, 5, 1};   // bend
-joint link4 = {4, 2, posE, 150, 700, 5, 1};   // rotate
-joint endEffector = {5, 2, posF, 350, 665, 5, 1};
+joint base = {0, 4, 400, 100, 700, 5, 1};    // rotate
+joint link1 = {1, 5, 400, 150, 700, 5, 1};   // bend reverse of link2
+joint link2 = {2, 3.5, 400, 150, 700, 5, 1}; // bend reverse of link1
+joint link3 = {3, 1, 400, 150, 700, 5, 1};   // bend
+joint link4 = {4, 2, 400, 150, 700, 5, 1};   // rotate
+joint endEffector = {5, 2, 400, 350, 665, 5, 1}; //close-open
 
 void moveMotor(joint *joint){
   Serial.println(joint->direction);
@@ -92,7 +63,7 @@ void moveMotor(joint *joint){
   if(newPosition < joint->min){
     newPosition = joint->min;
   }
-  pwm.setPWM(joint->pca9685pin, 0, newPosition);
+  pwm.setPWM(joint->onPCA9685pin, 0, newPosition);
   joint->jointPosition = newPosition;
   Serial.println(joint->jointPosition);
 }
@@ -105,32 +76,26 @@ void onDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) 
   Serial.println(myData.direction);
   if(myData.controlID == j2btn || myData.controlID == j1btn){
     base.direction = myData.direction;
-    delay(10);
     moveMotor(&base);
   }
   if(myData.controlID == j1x){
     link1.direction = myData.direction;
-    delay(10);
     moveMotor(&link1);
   }
   if(myData.controlID == j1y){
     link2.direction = myData.direction;
-    delay(10);
     moveMotor(&link2);
   }
   if(myData.controlID == j2x){
     link3.direction = myData.direction;
-    delay(10);
     moveMotor(&link3);
   }
   if(myData.controlID == j2y){
     link4.direction = myData.direction;
-    delay(10);
     moveMotor(&link4);
   }
   if(myData.controlID == btn1){
     endEffector.direction = myData.direction;
-    delay(10);
     moveMotor(&endEffector);
   }
 }
@@ -142,25 +107,12 @@ void setup() {
   esp_now_register_recv_cb(esp_now_recv_cb_t(onDataRecv));
   pwm.begin();
   pwm.setPWMFreq(FREQUENCY);
-  pwm.setPWM(0,0,posA);
-  pwm.setPWM(1,0,posB);
-  pwm.setPWM(2,0,posC);
-  pwm.setPWM(3,0,posD);
-  pwm.setPWM(4,0,posE);
-  pwm.setPWM(5,0,posF);
-
-/*
-  xTaskCreatePinnedToCore(
-    queue, 
-    "Task", 
-    5000, 
-    NULL, 
-    0, // if priority is too high (at max 10) then the main loop2 won't run
-    &TaskB, 
-    0
-  ); 
-  */
-
+  pwm.setPWM(0,0,400);
+  pwm.setPWM(1,0,400);
+  pwm.setPWM(2,0,400);
+  pwm.setPWM(3,0,400);
+  pwm.setPWM(4,0,400);
+  pwm.setPWM(5,0,400);
   Serial.println("Receiver Ready");
 }
  
--- wirelessControl/RobotESP32Transmitter/RobotESP32Transmitter.ino
+++ wirelessControl/RobotESP32Transmitter/RobotESP32Transmitter.ino
@@ -17,70 +17,41 @@ The joystick's position has either moved far enough outside of a defined neutral
 #include <esp_now.h>
 #include <Wire.h>
 
-TaskHandle_t Task1;
-SemaphoreHandle_t semaphore;
-QueueHandle_t queue;
-
-int semaphoreWaitTime = 60;
-int task0queue = 2;
-int task1queue = 2;
-int count = 2;
-
 // the joystick fluctuates even when youre not moving it. So this defines a range of values to ignore. Joystick values within the range (1600, 2200) will not trigger any movement.
-int neutral_upper_bound = 2200;
-int neutral_lower_bound = 1600;
-int neutral_upper_bound2 = 3000;
-int neutral_lower_bound2 = 1600;
-
-// using a delay to control the speed of the motors
+int neutral_upper_bound = 2400;
+int neutral_lower_bound = 1400;
+int neutral_upper_bound2 = 3200;
+int neutral_lower_bound2 = 1500;
 int delayTime = 50;
-// this tells the receiver how to increment a motors position by. Lower number = more 1int 1= 5;
 
+// controller IDs for buttons. (This info needs to be available to the receiver so it knows what I'm sending)
 int j1x = 601;
 int j1y = 602;
 int jbtn1 = 603;
 int j2x = 701;
 int j2y = 702;
 int j2btn = 703;
-
-
 int btn1 = 310;
 int btn2 = 320;
-int btn3 = 330;
-int btn4 = 340;
-int btn5 = 350;
-int btn6 = 360;
 
 // pin connections on the ESP32
-  // WORKS
 const int xOut = 34;
 const int yOut = 35;
 const int sel = 13; 
-  // WORKS
 const int x2Out = 33; 
 const int y2Out = 32;
 const int sel2 = 25; 
 
-  /* (6) buttons digital pins
-const int btn5_LB = 12; //originally was 26. Unsure if this pin will work
-const int btn5_RB = 14;
-const int btn0 = 2;
-const int btn1 = 4;
-const int btn2 = 15;
-const int btn3 = 9;     //originally was 27. Unsure if this pin will work
-*/
 // MAC Address of the ESP32 that will receive this data.
 uint8_t broadcastAddress[] = {0xD8, 0x13, 0x2A, 0x7E, 0xF5, 0x28}; //MAC addresses with 0x in front of each part
 
-// Define a data structure that accomodates the data being sent to the receiver. 
-//   Receiver should have the exact same data structure defined so that it has the tools to receive the data and manipulate it as needed.
+// Define a data structure that accomodates the data being sent to the receiver. Receiver should have the exact same data structure defined so it can receive it.
 typedef struct data_struct {
   int controlID; // this info tells the receiver to move the motor connected to this pin
   int direction;
 }; data_struct controllerData;
 
 esp_now_peer_info_t peerInfo;
-
 void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
@@ -91,7 +62,6 @@ void setup() {
   WiFi.mode(WIFI_STA);
   esp_now_init();
   esp_now_register_send_cb(onDataSent);
-
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;  
   peerInfo.encrypt = false;
@@ -99,19 +69,9 @@ void setup() {
 /*
   pinMode(btn0, INPUT_PULLUP);
   pinMode(btn1, INPUT_PULLUP);
-  pinMode(btn2, INPUT_PULLUP);
-  pinMode(btn3, INPUT_PULLUP);
-  pinMode(btn5_LB, INPUT_PULLUP);
-  pinMode(btn5_RB, INPUT_PULLUP);
 */
   pinMode(sel, INPUT_PULLUP);
   pinMode(sel2, INPUT_PULLUP);
-
-  neutral_upper_bound = analogRead(xOut) + 500;     // The new joystick2 has different values from joystick1, so the old neutral range does not work for joystick2. Instead I'll read the joystick positions on startup and +_ 500 to set the neutral zone.
-  neutral_upper_bound2 = analogRead(x2Out) + 500; // joystick starts at around 2400-2500 and joystick 1 starts around 1800-1900
-  neutral_lower_bound = analogRead(xOut) - 500;
-  neutral_lower_bound2 = analogRead(x2Out) - 500;
-
 /*  xTaskCreatePinnedToCore(
     taskB, 
     "Task", 
@@ -196,15 +156,6 @@ void taskB(void * parameter) {
 }*/
 
 void loop(){
-    /*defines the initial states of the controller. These are used as a point of reference to be able to tell when a button or joystick is pressed.
-  int btn0_state = digitalRead(btn0);
-  int btn1_state = digitalRead(btn1);
-  int btn2_state = digitalRead(btn2);
-  int btn3_state = digitalRead(btn3);
-  int btn5_LB_state = digitalRead(btn5_LB);
-  int btn5_RB_state = digitalRead(btn5_RB);
-  */
-
   // joystick1
   while(analogRead(xOut) > neutral_upper_bound){
     Serial.print(xOut);
@@ -249,9 +200,7 @@ void loop(){
     controllerData.direction = -1;
     sendData();
     delay(delayTime);
-    break;
     }
-  
   while(analogRead(x2Out) > neutral_upper_bound2){
     Serial.print(x2Out);
     Serial.print("  : ");
@@ -288,8 +237,7 @@ void loop(){
     sendData();
     delay(delayTime);
   }
-    //works
-  while(digitalRead(sel2) == LOW){ // directiond from !=Sel2_state. "== HIGH", "!=0" and "==1" do not work and triggers the send function constantly
+  while(digitalRead(sel2) == LOW){
     Serial.print("Joystick 2 pin: ");
     Serial.println((sel2));
     controllerData.controlID = j2btn;
@@ -297,7 +245,6 @@ void loop(){
     sendData();
     delay(delayTime);
   } 
-
 /*
   if(digitalRead(btn0) != btn0_state){
     Serial.println("elbow up");
@@ -310,61 +257,5 @@ void loop(){
     delay(delayTime);
     }
   }  
-  if(digitalRead(btn1) != btn1_state){
-    Serial.println("rotate the base to the left");
-    btn1_state = digitalRead(btn1);                 // btn1_state now equals the active state. 
-    while(btn1_state == digitalRead(btn1)){         // keep reading the button's state until it directions
-      btn1_state = digitalRead(btn1);
-    controllerData.controlID = 0;                                 // defines which of the robot arm's motor this controls by the pin its connected to on the PCA9685. 
-    controllerData.direction = -1;
-    esp_now_send(broadcastAddress, (uint8_t *) &controllerData, sizeof(controllerData));
-    delay(delayTime);
-    }
-  }
-  if(digitalRead(btn2) != btn2_state){
-    Serial.println("elbow down");
-    btn2_state = digitalRead(btn2);
-    while(btn2_state == digitalRead(btn2)){
-      btn2_state = digitalRead(btn2);
-    controllerData.controlID = 2;
-    controllerData.direction = -1;
-    esp_now_send(broadcastAddress, (uint8_t *) &controllerData, sizeof(controllerData)); 
-    delay(delayTime);
-    }
-  }
-  if(digitalRead(btn3) != btn3_state){
-    Serial.println("rotate the base to the right");
-    btn3_state = digitalRead(btn3);
-    while(btn3_state == digitalRead(btn3)){ 
-      btn3_state = digitalRead(btn3);
-    controllerData.controlID = 0;
-    controllerData.direction = 1;
-    esp_now_send(broadcastAddress, (uint8_t *) &controllerData, sizeof(controllerData)); 
-    delay(delayTime);
-    }
-  }
-  if(digitalRead(btn5_LB) != btn5_LB_state){
-    Serial.println("open claw ");
-    btn5_LB_state = digitalRead(btn5_LB);
-    while(btn5_LB_state == digitalRead(btn5_LB)){
-      btn5_LB_state = digitalRead(btn5_LB);
-    controllerData.controlID = 5;
-    controllerData.direction = -1;
-    esp_now_send(broadcastAddress, (uint8_t *) &controllerData, sizeof(controllerData));
-    delay(delayTime);
-    }
-  }
-  if(digitalRead(btn5_RB) != btn5_RB_state){
-    Serial.println("close claw");
-    btn5_RB_state = digitalRead(btn5_RB);
-    while(btn5_RB_state == digitalRead(btn5_RB)){
-      btn5_RB_state = digitalRead(btn5_RB);
-    controllerData.controlID = 5;
-    controllerData.direction = 1;
-    esp_now_send(broadcastAddress, (uint8_t *) &controllerData, sizeof(controllerData)); 
-    delay(delayTime);
-    }
-  }
   */
-  
 }
